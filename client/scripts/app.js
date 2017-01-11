01d9ffc564fda406c4ef05beaa3dc56ac096fb1b
Ai Shi <aishi@Ais-MacBook-Pro-2.local>
2017-01-10 20:50:11 -0800

    working on runing the server with the client

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,161 +1,236 @@
-// YOUR CODE HERE:
-var app = { };
 
-app.rooms = [];
+var app = {
 
-app.friends = [];
-// 
-app.allMessages = [];
+  //TODO: The current 'handleUsernameClick' function just toggles the class 'friend'
+  //to all messages sent by the user
+  server: 'http://127.0.0.1:3000',
+  username: 'anonymous',
+  roomname: 'lobby',
+  lastMessageId: 0,
+  friends: {},
+  messages: [],
 
-app.init = function() {
-  app.renderRoom();
+  init: function() {
+    // Get username
+    app.username = window.location.search.substr(10);
 
-  $(document).on('click', '.username', app.handleUsernameClick);
- 
-};
+    // Cache jQuery selectors
+    app.$message = $('#message');
+    app.$chats = $('#chats');
+    app.$roomSelect = $('#roomSelect');
+    app.$send = $('#send');
 
-app.send = function(message, success, dataType) {
+    // Add listeners
+    app.$chats.on('click', '.username', app.handleUsernameClick);
+    app.$send.on('submit', app.handleSubmit);
+    app.$roomSelect.on('change', app.handleRoomChange);
 
-  $.ajax({
-      // This is the url you should use to communicate with the parse API server.
-    url: 'http://127.0.0.1:3000',
-    type: 'POST',
-    data: JSON.stringify(message),
-    contentType: 'application/json',
-    success: function (data) {
-      console.log('chatterbox: Message sent');
-    },
-    error: function (data) {
-      // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
-      console.error('chatterbox: Failed to send message', data);
-    }
-  });
-};
+    // Fetch previous messages
+    app.startSpinner();
+    app.fetch(false);
 
-app.fetch = (url, data, success, dataType) => {
-  $.ajax({
-    url: url,
-    // data: {
-    //   order: '-createdAt'
-    // },
-    success: success,
-    dataType: dataType,
-  });
+    // Poll for new messages
+    setInterval(function() {
+      app.fetch(true);
+    }, 3000);
+  },
 
-};
+  send: function(message) {
+    app.startSpinner();
 
+    // POST the message to the server
+    $.ajax({
+      url: app.server,
+      type: 'POST',
+      data: JSON.stringify(message),
+      success: function (data) {
+        // Clear messages input
+        app.$message.val('');
 
-app.clearMessages = () => {
-  $('#chats').empty();
-  console.log('cleared');
-};
-
-// render a single piece of message as a div inside chats
-app.renderMessage = function(message) {  
-  $('#chats').append('<div class="chat"><h3 class="username" id="' + message.username + '">' + message.username + '</h3>' + '<p class="text">' 
-    + message.text + '</p>' + '<a class="roomname" onclick="functionCall()">' 
-    + message.roomname + '</a>' + '</div>');   
-  if (app.rooms.indexOf(message.roomname) === -1) {
-    app.rooms.push(message.roomname);
-    $('#rooms').append('<option class="roomname" value="' + message.roomname + ' ">' + message.roomname + '</option>');
-    console.log('new room added');
-  }
-};
-
-app.currentRoom;
-app.renderRoom = function(roomname) {
-
-  var a = roomname;
-  if (a === undefined) {
-    var undefinedInterval = setInterval(function() {
-
-      if (app.currentRoom !== a) {
-        clearInterval(undefinedInterval);
+        // Trigger a fetch to update the messages, pass true to animate
+        app.fetch();
+      },
+      error: function (error) {
+        console.error('chatterbox: Failed to send message', error);
       }
-      app.fetch('http://127.0.0.1:3000/classes/messages', {
+    });
+  },
 
-      }, function(result) {
-        app.clearMessages();
-        console.log('result', result);
-        for (var i = 0; i < result.results.length; i++) {
-          app.renderMessage(result.results[i]);
+  fetch: function(animate) {
+    $.ajax({
+      url: app.server,
+      type: 'GET',
+      // data: { order: '-createdAt' },
+      contentType: 'application/json',
+      success: function(data) {
+        // Don't bother if we have nothing to work with
+        if (!data.results || !data.results.length) { return; }
+
+        // Store messages for caching later
+        app.messages = data.results;
+
+        // Get the last message
+        var mostRecentMessage = data.results[data.results.length - 1];
+
+        // Only bother updating the DOM if we have a new message
+        if (mostRecentMessage.objectId !== app.lastMessageId) {
+          // Update the UI with the fetched rooms
+          app.renderRoomList(data.results);
+
+          // Update the UI with the fetched messages
+          app.renderMessages(data.results, animate);
+
+          // Store the ID of the most recent message
+          app.lastMessageId = mostRecentMessage.objectId;
         }
-      } );
-
-    }, 1000);    
-  } else {
-
-    a = a.trimRight();
-    var definedInterval = setInterval(function() {
-
-      if (app.currentRoom !== a) {
-        clearInterval(definedInterval);
+      },
+      error: function(error) {
+        console.error('chatterbox: Failed to fetch messages', error);
       }
-      app.fetch('http://127.0.0.1:3000/classes/messages', {
-      }, function(result) {
-        app.clearMessages();
-        for (var i = 0; i < result.results.length; i++) {
-          if (result.results[i].roomname === a) {
-            app.renderMessage(result.results[i]);
-          }
+    });
+  },
+
+  clearMessages: function() {
+    app.$chats.html('');
+  },
+
+  renderMessages: function(messages, animate) {
+    // Clear existing messages`
+    app.clearMessages();
+    app.stopSpinner();
+    if (Array.isArray(messages)) {
+      // Add all fetched messages that are in our current room
+      messages
+        .filter(function(message) {
+          return message.roomname === app.roomname ||
+                 app.roomname === 'lobby' && !message.roomname;
+        })
+        .forEach(app.renderMessage);
+    }
+
+    // Make it scroll to the top
+    if (animate) {
+      $('body').animate({scrollTop: '0px'}, 'fast');
+    }
+  },
+
+  renderRoomList: function(messages) {
+    app.$roomSelect.html('<option value="__newRoom">New room...</option>');
+
+    if (messages) {
+      var rooms = {};
+      messages.forEach(function(message) {
+        var roomname = message.roomname;
+        if (roomname && !rooms[roomname]) {
+          // Add the room to the select menu
+          app.renderRoom(roomname);
+
+          // Store that we've added this room already
+          rooms[roomname] = true;
         }
       });
+    }
 
-    }, 1000);    
+    // Select the menu option
+    app.$roomSelect.val(app.roomname);
+  },
+
+  renderRoom: function(roomname) {
+    // Prevent XSS by escaping with DOM methods
+    var $option = $('<option/>').val(roomname).text(roomname);
+
+    // Add to select
+    app.$roomSelect.append($option);
+  },
+
+  renderMessage: function(message) {
+    if (!message.roomname) {
+      message.roomname = 'lobby';
+    }
+
+    // Create a div to hold the chats
+    var $chat = $('<div class="chat"/>');
+
+    // Add in the message data using DOM methods to avoid XSS
+    // Store the username in the element's data attribute
+    var $username = $('<span class="username"/>');
+    $username.text(message.username + ': ').attr('data-roomname', message.roomname).attr('data-username', message.username).appendTo($chat);
+
+    // Add the friend class
+    if (app.friends[message.username] === true) {
+      $username.addClass('friend');
+    }
+
+    var $message = $('<br><span/>');
+    $message.text(message.text).appendTo($chat);
+
+    // Add the message to the UI
+    app.$chats.append($chat);
+
+  },
+
+  handleUsernameClick: function(event) {
+
+    // Get username from data attribute
+    var username = $(event.target).data('username');
+
+    if (username !== undefined) {
+      // Toggle friend
+      app.friends[username] = !app.friends[username];
+
+      // Escape the username in case it contains a quote
+      var selector = '[data-username="' + username.replace(/"/g, '\\\"') + '"]';
+
+      // Add 'friend' CSS class to all of that user's messages
+      var $usernames = $(selector).toggleClass('friend');
+    }
+  },
+
+  handleRoomChange: function(event) {
+
+    var selectIndex = app.$roomSelect.prop('selectedIndex');
+    // New room is always the first option
+    if (selectIndex === 0) {
+      var roomname = prompt('Enter room name');
+      if (roomname) {
+        // Set as the current room
+        app.roomname = roomname;
+
+        // Add the room to the menu
+        app.renderRoom(roomname);
+
+        // Select the menu option
+        app.$roomSelect.val(roomname);
+      }
+    } else {
+      app.startSpinner();
+      // Store as undefined for empty names
+      app.roomname = app.$roomSelect.val();
+    }
+    // Rerender messages
+    app.renderMessages(app.messages);
+  },
+
+  handleSubmit: function(event) {
+    var message = {
+      username: app.username,
+      text: app.$message.val(),
+      roomname: app.roomname || 'lobby'
+    };
+
+    app.send(message);
+
+    // Stop the form from submitting
+    event.preventDefault();
+  },
+
+  startSpinner: function() {
+    $('.spinner img').show();
+    $('form input[type=submit]').attr('disabled', 'true');
+  },
+
+  stopSpinner: function() {
+    $('.spinner img').fadeOut('fast');
+    $('form input[type=submit]').attr('disabled', null);
   }
-  app.currentRoom = a;
 };
-// app.renderRoom = function(roomname) {
-//   var $option = $('<option/>').val(roomname).text(roomname);
-
-//   app.$roomSelect.append($option);
-// };
-// app.renderRoomList = function(messages) {
-//   app.$roomSelect.html('<option value="__newRoom">New room...</option></select>');
-
-//   if (messages) {
-//     messages.forEach(function(message) {
-//       var roomname = message.roomname;
-//       if (roomname) {
-//         app.renderRoom(roomname);
-//       }
-//     });
-//   }
-
-// };
-
-
-app.handleUsernameClick = function(event) {
-  var name = event.currentTarget.innerText;
-  if (app.friends.indexOf(name) === -1) {
-    app.friends.push(name);
-    var id = '#' + name;
-    $(id).addClass('.friend');
-  } else {
-    $(id).removeClass('.friend');
-  }
-};
-
-app.chooseRoom = function() {
-  app.currentRoom = $(this).message.roomname;
-  console.log('room selected');
-};
-
-// '<script>document.body.style.backgroundImage = "url( 'http://images.8tracks.com/cover/i/009/493/548/Haha_i_say_that_all_the_time__406a982d46ccc523799e6aef45080387_1_-5889.png?rect=100,0,711,711&q=98&fm=jpg&fit=max&w=640&h=640' )"</script>;'
-app.handleSubmit = function() {
-  var message = {};
-  message.text = $('#message').val();
-  message.username = 'Carlos Danger';
-  if ($('#addRoom').val()) {
-    message.roomname = $('#addRoom').val();
-  } else if (app.currentRoom) {
-    message.roomname = app.currentRoom;
-  } else {
-    message.roomname = 'lobby';
-  }
-  app.send(message);
-  app.renderRoom(message.roomname);
-};
-
-app.init();
-
--- server/request-handler.js
+++ server/request-handler.js
@@ -18,32 +18,60 @@ var defaultCorsHeaders = {
   'access-control-allow-headers': 'content-type, accept',
   'access-control-max-age': 10 // Seconds.
 };
+
+var fs = require('fs');
+
 var port = 3000;
 var ip = '127.0.0.1';
 var serverUrl = 'http://127.0.0.1:3000/classes/messages';
-// the data stored in the server
 var data = [];
 
+// var requestHandler = function(request, response) {
+
+//   var headers = request.headers;
+//   var method = request.method;
+//   var url = ''.concat('http://', ip, ':', port, request.url, 'classes/messages');
+//   var results = [];
+
+//   var result = {
+//     headers: headers,
+//     method: method,
+//     url: url,
+//     results: data,
+//   };
+
+//   if (url !== serverUrl) {
+//     response.writeHead(404, {'Content-type': 'application/json'});
+//     response.end();
+//   } else if (method === 'GET') {   
+//     response.writeHead(200, {'Content-type': 'application/json'});
+//     response.end(JSON.stringify(result));
+//   } else if (method === 'POST') {
+//     var requestBody = '';
+//     request.on('error', function(err) {
+//       console.error(err);
+//     });
+//     request.on('data', function(data) {
+//       response.writeHead(201, {'Content-Type': 'application/json'});
+//       requestBody += data;
+//       response.end();        
+//     });
+//     request.on('end', function() {
+//       var message = JSON.parse(requestBody);
+//       data.push(message); 
+//       response.writeHead(201, {'Content-Type': 'application/json'});
+//       response.end();
+//     });
+//   } else if (method === 'OPTIONS') {
+//     result.headers = defaultCorsHeaders;
+//   }
+
+// };
 var requestHandler = function(request, response) {
-  // Request and Response come from node's http module.
-  //
-  // They include information about both the incoming request, such as
-  // headers and URL, and about the outgoing response, such as its status
-  // and content.
-  //
-  // Documentation for both request and response can be found in the HTTP section at
-  // http://nodejs.org/documentation/api/
-
-  // Do some basic logging.
-  //
-  // Adding more logging to your server can be an easy way to get passive
-  // debugging help, but you should always be careful about leaving stray
-  // console.logs in your code.
-
 
   var headers = request.headers;
   var method = request.method;
-  var url = ''.concat('http://', ip, ':', port, request.url);
+  var url = ''.concat('http://', ip, ':', port, request.url, 'classes/messages');
   var results = [];
 
   var result = {
@@ -52,54 +80,39 @@ var requestHandler = function(request, response) {
     url: url,
     results: data,
   };
-
+  console.log('request url: ', request.url);
+  console.log('url: ', url);
+  console.log('serverUrl: ', serverUrl);
+  var writeStrem = fs.createWriteStream('./output');
   if (url !== serverUrl) {
-    response.writeHead(404, {'Content-type': 'text/plain'});
+    console.log('Got ya!');
+    response.writeHead(404, {'Content-type': 'application/json'});
     response.end();
-  } else if (method === 'GET') {
-    response.writeHead(200, {'Content-type': 'text/plain'});
+  } else if (method === 'GET') {   
+    response.writeHead(200, {'Content-type': 'application/json'});
     response.end(JSON.stringify(result));
   } else if (method === 'POST') {
-    if (url === serverUrl) {    
-      var requestBody = '';
-      request.on('data', function(data) {
-        response.writeHead(201, {'Content-Type': 'text/plain'});
-        requestBody += data;
-        response.end();        
-      });
-      request.on('end', function() {
-        var message = JSON.parse(requestBody);
-        data.push(message); 
-        response.writeHead(201, {'Content-Type': 'text/plain'});
-        response.end();
-      });
-    }
+    var requestBody = '';
+    request.on('error', function(err) {
+      console.error(err);
+    });
+    request.on('data', function(data) {
+      response.writeHead(201, {'Content-Type': 'application/json'});
+      requestBody += data;
+      response.end();        
+    });
+    request.on('end', function() {
+      var message = JSON.parse(requestBody);
+      console.log(message);
+      data.push(message); 
+      response.writeHead(201, {'Content-Type': 'application/json'});
+      response.end();
+    });
   } else if (method === 'OPTIONS') {
     result.headers = defaultCorsHeaders;
   }
 
-  // See the note below about CORS headers.
-
-  // Tell the client we are sending them plain text.
-  //
-  // You will need to change this if you are sending something
-  // other than plain text, like JSON or HTML.
-  // headers['Content-Type'] = 'text/plain';
-
-  // .writeHead() writes to the request line and headers of the response,
-  // which includes the status and all headers.
-  // response.writeHead(statusCode, headers);
-  // Make sure to always call response.end() - Node may not send
-  // anything back to the client until you do. The string you pass to
-  // response.end() will be the body of the response - i.e. what shows
-  // up in the browser.
-  //
-
-
-  // Calling .end "flushes" the response's internal buffer, forcing
-  // node to actually send all the data over to the client.
 };
-
 // These headers will allow Cross-Origin Resource Sharing (CORS).
 // This code allows this server to talk to websites that
 // are on different domains, for instance, your chat client.
@@ -109,6 +122,5 @@ var requestHandler = function(request, response) {
 //
 // Another way to get around this restriction is to serve you chat
 // client from this domain by setting up static file serving.
-
 
 exports.requestHandler = requestHandler;
